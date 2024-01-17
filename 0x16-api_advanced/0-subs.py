5c73454bb8f46917d40edf044f31b345286e2324
Kiruti01 <mikekirutic@gmail.com>
2024-01-17 11:58:58 +0300

    edit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
-'''Module that contains the function number_of_subscribers'''
+'''A module containing functions for working with the Reddit API.
+'''
 import requests
-from sys import argv
+
+
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
 
 
 def number_of_subscribers(subreddit):
-    '''A function that returns the number of subs for a subreddit'''
-    user_agent = {'User-Agent': 'Bright'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-
-    try:
-        response = requests.get(url, headers=user_agent)
-        response.raise_for_status()  # Raise an exception for bad responses
-
-    data = response.json().get('data')
-    subscribers = data.get('subscribers') if data else 0
-
-        return subscribers
-    except requests.RequestException as e:
-        print("Error: {}".format(e))
-        return 0
-
-    if __name__ == "__main__":
-        if len(argv) == 2:
-            subreddit_name = argv[1]
-            subscribers_count = number_of_subscribers(subreddit_name)
-            print("The subreddit '{}' has {} "
-                    "subscribers.".format(subreddit_name, subscribers_count))
-        else:
-            print("Usage: python script.py <subreddit_name>")
+    '''Retrieves the number of subscribers in a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    res = requests.get(
+        '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
+    return 0
