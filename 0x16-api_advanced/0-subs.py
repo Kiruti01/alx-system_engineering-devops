64923fcc5fe271c56375c89402e05ac103505f5f
Kiruti01 <mikekirutic@gmail.com>
2024-01-18 12:55:16 +0300

    18th

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
-'''A module containing functions for working with the Reddit API.
-'''
+""" a function that queries the Reddit API """
+
+import sys
 import requests
 
 
-BASE_URL = 'https://www.reddit.com'
-'''Reddit's base API URL.
-'''
-
-
 def number_of_subscribers(subreddit):
-    '''Retrieves the number of subscribers in a given subreddit.
-    '''
-    api_headers = {
-        'Accept': 'application/json',
-        'User-Agent': ' '.join([
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-            'AppleWebKit/537.36 (KHTML, like Gecko)',
-            'Chrome/97.0.4692.71',
-            'Safari/537.36',
-            'Edg/97.0.1072.62'
-        ])
-    }
-    res = requests.get(
-        '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
-        headers=api_headers,
-        allow_redirects=False
-    )
-    if res.status_code == 200:
-        return res.json()['data']['subscribers']
-    return 0
+    """a function that queries the Reddit API"""
+    headers = {"User-Agent": "MyBot/0.0.1"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    # Make a GET request to the Reddit API
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,41 +1,13 @@
 #!/usr/bin/python3
-'''A module containing functions for working with the Reddit API.
-'''
-import requests
-
-
-BASE_URL = 'https://www.reddit.com'
-'''Reddit's base API URL.
-'''
-
+# a function that queries the Reddit API
+# Return - 0 on failire, 1 on success
+import sys
+from api import number_of_subscribers
 
 def top_ten(subreddit):
-    '''Retrieves the title of the top ten posts from a given subreddit.
-    '''
-    api_headers = {
-        'Accept': 'application/json',
-        'User-Agent': ' '.join([
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-            'AppleWebKit/537.36 (KHTML, like Gecko)',
-            'Chrome/97.0.4692.71',
-            'Safari/537.36',
-            'Edg/97.0.1072.62'
-        ])
-    }
-    sort = 'top'
-    limit = 10
-    res = requests.get(
-        '{}/r/{}/.json?sort={}&limit={}'.format(
-            BASE_URL,
-            subreddit,
-            sort,
-            limit
-        ),
-        headers=api_headers,
-        allow_redirects=False
-    )
-    if res.status_code == 200:
-        for post in res.json()['data']['children'][0:10]:
-            print(post['data']['title'])
+    try:
+        return number_of_subscribers
+    if subreddit == 0:
+        print("invalid subreddit")
     else:
-        print(None)
+        print("valid reddit")
